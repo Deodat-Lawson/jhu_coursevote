879a3661dc10f84ced294dcb8bf5d068a08206cb
timot <timothylinziqi@gmail.com>
2024-11-25 23:09:50 -0500

    connected to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,43 @@
 import Link from "next/link";
-
-export default function HomePage() {
-
-  const mockUrl = [
-    "https://utfs.io/f/7CCGyytM9ORCCFJ2C5euiXWMRZ8pT590k71jgUAnoEwPJy2F",
-  ];
-
-  const mockImages = mockUrl.map((url, index) => ({
-    id: index + 1,
-    url,
-  }));
+import { db } from "~/server/db";
 
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="flex flex-wrap gap-4">
-        {mockImages.map((image) => (
-          <div key={image.id} className="m-2">
-            <img
-              src={image.url}
-              alt="mock"
-              className="w-32 h-32 object-cover rounded-lg"
-            />
-          </div>
-        ))}
+export default async function HomePage() {
+    const posts = await db.query.posts.findMany();
 
 
-      Hello, development in progress!
-      </div>
-    </main>
-  );
+
+    const mockUrl = [
+        "https://utfs.io/f/7CCGyytM9ORCCFJ2C5euiXWMRZ8pT590k71jgUAnoEwPJy2F",
+    ];
+
+    const mockImages = mockUrl.map((url, index) => ({
+        id: index + 1,
+        url,
+    }));
+
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+            <div className="flex flex-wrap gap-4">
+                {posts.map((post) => (
+                    <div key={post.id} className="m-2">
+                        <h2 className="text-xl font-bold">{post.name}</h2>
+                    </div>
+                ))}
+
+                {mockImages.map((image) => (
+                    <div key={image.id} className="m-2">
+                        <img
+                            src={image.url}
+                            alt="mock"
+                            className="w-32 h-32 object-cover rounded-lg cursor-pointer"
+                        />
+                    </div>
+                ))}
+
+                Hello, development in progress!
+            </div>
+        </main>
+    );
 }
